8c2eb545a3cd88fe032f7eeb9d4a29dda4d67fa8
Molly Herforth <molly@Mollys-MacBook-Pro.local>
2024-11-07 15:38:51 -0500

    created a vulnerable flask app to trigger something.

--- scripts/trigger_safety.py
+++ scripts/trigger_safety.py
@@ -1,1 +1,11 @@
 #this is just a file to trigger a safety scan
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
