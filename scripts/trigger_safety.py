9773cfc1667346ab2742604d11b80e7af076f5ac
Molly Herforth <molly@mac.mynetworksettings.com>
2024-11-08 13:08:03 -0500

    created a new vulnerable file that imports vulnerable 'urllib3' package to test safety scan detection and generate alerts in code scanning alerts.

--- scripts/trigger_safety.py
+++ scripts/trigger_safety.py
@@ -1,11 +1,7 @@
 #this is just a file to trigger a safety scan
 
-import requests
+import urllib3
 
-def fetch_data():
-    url = 'https://example.com'
-    response = requests.get(url)
-    print(f"Response status code: {response.status_code}")
-
-if __name__ == "__main__":
-    fetch_data()
+http = urllib3.PoolManager()
+response = http.request('GET', 'https://httpbin.org/ip')
+print(response.data)
