5b36ba497f6edd4bb2478be9c77d10c49831c903
Molly Herforth <molly@mac.mynetworksettings.com>
2024-11-08 11:47:08 -0500

    created a new vulnerable file that imports vulnerable requests package to test safety scan detection and generate alerts in code scanning alerts

--- scripts/trigger_safety.py
+++ scripts/trigger_safety.py
@@ -1,11 +1,11 @@
 #this is just a file to trigger a safety scan
-from flask import Flask
 
-app = Flask(__name__)
+import requests
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+def fetch_data():
+    url = 'https://example.com'
+    response = requests.get(url)
+    print(f"Response status code: {response.status_code}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    fetch_data()
